b284405102cc32756cbfb8dc5e7e84e91112b127
LudoO <ludovic.valente@gmail.com>
2024-12-09 14:41:37 +0100

    Update Day03b.kt

--- src/Day03b.kt
+++ src/Day03b.kt
@@ -1,30 +1,36 @@
 // solution 106780429
 
-val rex = "mul\\((\\d+),(\\d+)\\)".toRegex()
-val rexdo = "do\\(\\)".toRegex()
-val rexdont = "don't\\(\\)".toRegex()
-val disablesMul = "don't\\(\\).+?do\\(\\)".toRegex()
+class Day03b(){
+    val rex = "mul\\((\\d+),(\\d+)\\)".toRegex()
+    val rexdo = "do\\(\\)".toRegex()
+    val rexdont = "don't\\(\\)".toRegex()
+    val disablesMul = "don't\\(\\).+?do\\(\\)".toRegex()
+
+   fun main() {
+
+        var testInputs = readInput("Day03_test")
+        val line = testInputs.joinToString ("") { it }
+            println("File has ${testInputs.size} lines")
+    
+        var input = line.replace(disablesMul, "")
+        val ido = rexdo.find( input ).let { it?.range?.first ?: 0 }
+    
+        val idont = rexdont.find( input ).let { it?.range?.first ?: 0 }
+        if (idont > ido){
+            //remove last dont and after
+            println("ends with dont")
+            input=input.substring(0, idont)
+        }
+    
+        val total = rex.findAll(input)
+            .map { it.groups[1]?.value!!.toInt() * it.groups[2]?.value!!.toInt() }
+            .reduce { acc, next -> acc + next }
+    
+        println("total = $total")
+
+  }
+}
 
 fun main() {
-
-    var testInputs = readInput("Day03_test")
-    val line = testInputs.joinToString ("") { it }
-        println("File has ${testInputs.size} lines")
-
-    var input = line.replace(disablesMul, "")
-    val ido = rexdo.find( input ).let { it?.range?.first ?: 0 }
-
-    val idont = rexdont.find( input ).let { it?.range?.first ?: 0 }
-    if (idont > ido){
-        //remove last dont and after
-        println("ends with dont")
-        input=input.substring(0, idont)
-    }
-
-    val total = rex.findAll(input)
-        .map { it.groups[1]?.value!!.toInt() * it.groups[2]?.value!!.toInt() }
-        .reduce { acc, next -> acc + next }
-
-    println("total = $total")
-
+     Day03b().main()
 }
