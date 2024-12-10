4ddde6de0d77a58b7967a18b78e3c83226b8949b
LudoO <ludovic.valente@gmail.com>
2024-12-10 07:16:11 +0100

    feat:setup day 10 star 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -8,57 +8,65 @@ class Day10 {
     val DEBUG=true;
 
     fun main() {
-        val input = File("src/Day10_input.txt").readLines()
-        val result = sumTrailheadScores(input)
-        println("Sum of trailhead scores: $result")
-    }
+        fun parseMap(input: List<String>): Array<IntArray> {
+            return input.map { line ->
+                line.map { it.digitToInt() }.toIntArray()
+            }.toTypedArray()
+        }
 
-    fun sumTrailheadScores(input: List<String>): Int {
-        val grid = input.map { it.map { c -> c.digitToInt() } }
-        val rows = grid.size
-        val cols = grid[0].size
+        fun isTrailhead(map: Array<IntArray>, x: Int, y: Int): Boolean {
+            val height = map[y][x]
+            val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+            return directions.all { (dx, dy) ->
+                val nx = x + dx
+                val ny = y + dy
+                nx !in map[0].indices || ny !in map.indices || map[ny][nx] < height
+            }
+        }
 
-        fun isValid(r: Int, c: Int) = r in 0 until rows && c in 0 until cols
-
-        val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
-
-        fun bfs(startR: Int, startC: Int): Int {
-            val visited = Array(rows) { BooleanArray(cols) }
+        fun calculateRating(map: Array<IntArray>, startX: Int, startY: Int): Int {
+            val visited = mutableSetOf<Pair<Int, Int>>()
             val queue = ArrayDeque<Triple<Int, Int, Int>>()
-            queue.add(Triple(startR, startC, 0))
-            visited[startR][startC] = true
-            var score = 0
+            queue.add(Triple(startX, startY, map[startY][startX]))
 
             while (queue.isNotEmpty()) {
-                val (r, c, height) = queue.removeFirst()
-
-                if (grid[r][c] == 9) {
-                    score++
+                val (x, y, height) = queue.removeFirst()
+                if (visited.add(x to y)) {
+                    val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1)
+                    for ((dx, dy) in directions) {
+                        val nx = x + dx
+                        val ny = y + dy
+                        if (nx in map[0].indices && ny in map.indices && map[ny][nx] > height) {
+                            queue.add(Triple(nx, ny, map[ny][nx]))
+                        }
+                    }
                 }
+            }
+            return visited.size
+        }
 
-                for ((dr, dc) in directions) {
-                    val newR = r + dr
-                    val newC = c + dc
-                    if (isValid(newR, newC) && !visited[newR][newC] && grid[newR][newC] == height + 1) {
-                        queue.add(Triple(newR, newC, height + 1))
-                        visited[newR][newC] = true
+        fun part2(input: List<String>): Int {
+            val map = parseMap(input)
+            var totalRating = 0
+
+            for (y in map.indices) {
+                for (x in map[0].indices) {
+                    if (isTrailhead(map, x, y)) {
+                        totalRating += calculateRating(map, x, y)
                     }
                 }
             }
 
-            return score
+            return totalRating
         }
 
-        var totalScore = 0
-        for (r in 0 until rows) {
-            for (c in 0 until cols) {
-                if (grid[r][c] == 0) {
-                    totalScore += bfs(r, c)
-                }
-            }
-        }
+        val testInput = readInput("Day10_star2_sample")
+        val res = part2(testInput)
+        println("Result sample star 2 : $res")
+        check(res == 81)
 
-        return totalScore
+        val input = readInput("Day10_input")
+        println(part2(input))
     }
 }
 
